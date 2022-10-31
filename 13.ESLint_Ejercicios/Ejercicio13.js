a1795da50a781c9b2a6e09868995b9116b490962
Japarraes <110259395+Japarraes@users.noreply.github.com>
2022-10-31 13:38:26 +0100

    Add files via upload

---
+++ 13.ESLint_Ejercicios/Ejercicio13.js
@@ -0,0 +1,26 @@
+// Strings
+let str = "Hola soy un texto"
+console.log(str)
+
+// String con entrcomillado dentro
+let str_comillas = "El otro día me dijo: \"No te lo crees ni tú\""
+let str_comillas_simples = "El otro día me dijo: \"No te lo crees ni tú\"" //eslint-disable-line
+let str_comillas_simples2 = "El otro día me dijo: \"No te lo crees ni tú\""
+console.log(str_comillas)
+console.log(str_comillas_simples2)
+console.log(str_comillas_simples2)
+
+// String con comillas invertidas (backticks)
+let str_backticks = "Esto es un texto con backticks"
+console.log(str_backticks)
+
+let nombre = "Juan"
+console.log(`Hola ${nombre}, cómo estas hoy`)
+
+// Plantilla html
+let plantilla = `
+<html>
+    <h1>Página web de ${nombre}</h1>
+    <p>Esto es una página web</P>
+</html>`
+console.log(plantilla)
